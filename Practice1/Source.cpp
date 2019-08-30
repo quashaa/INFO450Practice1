70c7bd583d11b1a7b6324e88f41776d482cf6d37
Andrew Quash <quashaa@vcu.edu>
2019-08-29 21:02:40 -0400

    excelent version

--- Practice1/Source.cpp
+++ Practice1/Source.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 int main()
 {
+	float value = 1;
+	while (value != 0)
+	{
+		cout << "Please enter a value below:" << endl;
+		cin >> value;
+		if (value == 0)
+			break;
+		cout << "\nThe square of your value is " << pow(value, 2) << "\n" << endl;
 
+	}
+
+	system("pause");
 
 	return 0;
 }
